d89602811f2c7b1c42761cba1513c4420507fcde
seyla <vichetseylait@gmail.com>
2021-04-02 10:38:43 +0700

    first commit

---
+++ EasyADO/Parameter.cs
@@ -0,0 +1,10 @@
+﻿using System.Data;
+namespace EasyADO
+{
+    public class Parameter
+    {
+        public string ParameterName { get; set; }
+        public SqlDbType ParameterType { get; set; }
+        public dynamic ParameterValue { get; set; }
+    }
+}
